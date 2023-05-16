84b84302fc99ec6308053584ba54c8b6c8f44eaa
Nils Ratnaweera <rata@zhaw.ch>
2023-05-16 11:41:10 +0200

    udpate

--- 03_Shiny_GDP/eda.R
+++ 03_Shiny_GDP/eda.R
@@ -1,30 +1,17 @@
 
 library(tidyverse)
 library(plotly)
-workshop_data <- read_csv("03_Shiny_GDP/03_lebenserwartung/data/prepared/workshop_data.csv")
+workshop_data <- read_csv("03_lebenserwartung/data/prepared/workshop_data.csv")
 
 
-ggplotly(workshop_data %>% 
-           # filter(Country == "Eswatini") %>% 
-  ggplot(aes(x = Year, y = GdpPerCapita, color = Region, group = Country)) +
-  geom_line()  +
-  scale_y_log10())
+workshop_data %>% 
+  ggplot(aes(x = GdpPerCapita, y = LifeExpectancy, color = Region, group = Country)) +
+  geom_path()  +
+  scale_x_log10() +
+  theme(legend.position = "none")
 
 
 
-
-install.packages("ggvis")
-
-library(ggvis)
-
-
-myggvis <- function(dataset, year){
-  dataset %>% 
-    filter(Year == year) %>% 
-    ggvis(x = ~LifeExpectancy, y = ~log10(GdpPerCapita), size = ~Population, fill = ~Region) %>% 
-    layer_points() %>% 
-    ggvis::hide_legend("size")
-}
 
 myggvis(workshop_data, 1987)
 
